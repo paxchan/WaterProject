488c81e5bd314d94358b0d01fdd3e0daf1adfcd5
Channing Paxman <channing.paxman@gmail.com>
2025-04-02 15:10:15 -0600

    the rest

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react';
 import './App.css';
-import ProjectList from './ProjectList';
+import { CartProvider } from './context/CartContext';
+import CartPage from './pages/CartPage';
+import DonatePage from './pages/DonatePage';
+import ProjectsPage from './pages/ProjectsPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <ProjectList />
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<ProjectsPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route
+              path="/donate/:projectName/:projectId"
+              element={<DonatePage />}
+            />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </Router>
+      </CartProvider>
     </>
   );
 }
